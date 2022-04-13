4c19a543db55b7f14fcd11e032dd43161cba12e5
avrham <aneeman2010@gmail.com>
2022-04-13 20:33:07 +0300

    final changes

--- src/components/sideBar/index.js
+++ src/components/sideBar/index.js
@@ -72,100 +72,98 @@ function SideBar() {
     };
 
     return (
-        <Desktop>
-            <div className="sidebar-container">
-                <div className="upper-icons">
+        <div className="sidebar-container">
+            <div className="upper-icons">
+                <Link to={pathControllers.overview}>
+                    <img src={Logo} width="30" height="25" className="logoimg" alt="logo" />
+                </Link>
+                <div className="item-wrapper">
                     <Link to={pathControllers.overview}>
-                        <img src={Logo} width="30" height="25" className="logoimg" alt="logo" />
+                        <div className="icon">
+                            <div className={state.route === 'overview' ? 'circle-nav-item checked' : 'circle-nav-item'}>
+                                {state.route === 'overview' ? (
+                                    <img src={overviewIconActive} alt="overviewIconActive" width="20" height="20"></img>
+                                ) : (
+                                    <img src={overviewIcon} alt="overviewIcon" width="20" height="20"></img>
+                                )}
+                            </div>
+                        </div>
                     </Link>
-                    <div className="item-wrapper">
-                        <Link to={pathControllers.overview}>
-                            <div className="icon">
-                                <div className={state.route === 'overview' ? 'circle-nav-item checked' : 'circle-nav-item'}>
-                                    {state.route === 'overview' ? (
-                                        <img src={overviewIconActive} alt="overviewIconActive" width="20" height="20"></img>
-                                    ) : (
-                                        <img src={overviewIcon} alt="overviewIcon" width="20" height="20"></img>
-                                    )}
-                                </div>
-                            </div>
-                        </Link>
-                        <p className={state.route === 'overview' ? 'checked' : 'name'}>Overview</p>
-                    </div>
-                    <div className="item-wrapper">
-                        <Link to={pathControllers.factoriesList}>
-                            <div className="icon">
-                                <div className={state.route === 'factories' ? 'circle-nav-item checked' : 'circle-nav-item'}>
-                                    {state.route === 'factories' ? (
-                                        <img src={factoriesIconActive} alt="factoriesIconActive" width="20" height="20"></img>
-                                    ) : (
-                                        <img src={factoriesIcon} alt="factoriesIcon" width="20" height="20"></img>
-                                    )}
-                                </div>
-                            </div>
-                        </Link>
-                        <p className={state.route === 'factories' ? 'checked' : 'name'}>Factories</p>
-                    </div>
-
-                    <div className="item-wrapper">
-                        <Link to={pathControllers.users}>
-                            <div className="icon">
-                                <div className={state.route === 'users' ? 'circle-nav-item checked' : 'circle-nav-item'}>
-                                    {state.route === 'users' ? (
-                                        <img src={usersIconActive} alt="usersIconActive" width="20" height="20"></img>
-                                    ) : (
-                                        <img src={usersIcon} alt="usersIcon" width="20" height="20"></img>
-                                    )}
-                                </div>
-                            </div>
-                        </Link>
-                        <p className={state.route === 'users' ? 'checked' : 'name'}>Users</p>
-                    </div>
+                    <p className={state.route === 'overview' ? 'checked' : 'name'}>Overview</p>
                 </div>
-                <div className="bottom-icons">
-                    <Menu onClick={handleClick} className="app-menu" mode="vertical" triggerSubMenuAction="click">
-                        <SubMenu
-                            key="subMenu"
-                            icon={
-                                <div className="sub-icon-wrapper">
-                                    <img src={botUrl} width={25} height={25} alt="bot"></img>
+                <div className="item-wrapper">
+                    <Link to={pathControllers.factoriesList}>
+                        <div className="icon">
+                            <div className={state.route === 'factories' ? 'circle-nav-item checked' : 'circle-nav-item'}>
+                                {state.route === 'factories' ? (
+                                    <img src={factoriesIconActive} alt="factoriesIconActive" width="20" height="20"></img>
+                                ) : (
+                                    <img src={factoriesIcon} alt="factoriesIcon" width="20" height="20"></img>
+                                )}
+                            </div>
+                        </div>
+                    </Link>
+                    <p className={state.route === 'factories' ? 'checked' : 'name'}>Factories</p>
+                </div>
+
+                <div className="item-wrapper">
+                    <Link to={pathControllers.users}>
+                        <div className="icon">
+                            <div className={state.route === 'users' ? 'circle-nav-item checked' : 'circle-nav-item'}>
+                                {state.route === 'users' ? (
+                                    <img src={usersIconActive} alt="usersIconActive" width="20" height="20"></img>
+                                ) : (
+                                    <img src={usersIcon} alt="usersIcon" width="20" height="20"></img>
+                                )}
+                            </div>
+                        </div>
+                    </Link>
+                    <p className={state.route === 'users' ? 'checked' : 'name'}>Users</p>
+                </div>
+            </div>
+            <div className="bottom-icons">
+                <Menu onClick={handleClick} className="app-menu" mode="vertical" triggerSubMenuAction="click">
+                    <SubMenu
+                        key="subMenu"
+                        icon={
+                            <div className="sub-icon-wrapper">
+                                <img src={botUrl} width={25} height={25} alt="bot"></img>
+                            </div>
+                        }
+                    >
+                        <Menu.ItemGroup
+                            title={
+                                <div className="header-menu">
+                                    <div className="company-logo">
+                                        <img src={state?.companyLogo || Logo} width="20" height="20" className="logoimg" alt="companyLogo" />
+                                    </div>
+                                    <p>{localStorage.getItem(LOCAL_STORAGE_USER_NAME)}</p>
                                 </div>
                             }
                         >
-                            <Menu.ItemGroup
-                                title={
-                                    <div className="header-menu">
-                                        <div className="company-logo">
-                                            <img src={state?.companyLogo || Logo} width="20" height="20" className="logoimg" alt="companyLogo" />
-                                        </div>
-                                        <p>{localStorage.getItem(LOCAL_STORAGE_USER_NAME)}</p>
-                                    </div>
-                                }
-                            >
-                                <Menu.Item key={1} className="customclass">
-                                    <div className="item-wrapp">
-                                        <img src={accountIcon} width="15" height="15" alt="accountIcon" />
-                                        <p>Settings</p>
-                                    </div>
-                                </Menu.Item>
-                                <Menu.Item key={2}>
-                                    <div className="item-wrapp">
-                                        <img src={supportIcon} width="15" height="15" alt="supportIcon" />
-                                        <p>Support</p>
-                                    </div>
-                                </Menu.Item>
-                                <Menu.Item key={3}>
-                                    <div className="item-wrapp">
-                                        <img src={logoutIcon} width="15" height="15" alt="logoutIcon" />
-                                        <p>Log out</p>
-                                    </div>
-                                </Menu.Item>
-                            </Menu.ItemGroup>
-                        </SubMenu>
-                    </Menu>
-                </div>
+                            <Menu.Item key={1} className="customclass">
+                                <div className="item-wrapp">
+                                    <img src={accountIcon} width="15" height="15" alt="accountIcon" />
+                                    <p>Settings</p>
+                                </div>
+                            </Menu.Item>
+                            <Menu.Item key={2}>
+                                <div className="item-wrapp">
+                                    <img src={supportIcon} width="15" height="15" alt="supportIcon" />
+                                    <p>Support</p>
+                                </div>
+                            </Menu.Item>
+                            <Menu.Item key={3}>
+                                <div className="item-wrapp">
+                                    <img src={logoutIcon} width="15" height="15" alt="logoutIcon" />
+                                    <p>Log out</p>
+                                </div>
+                            </Menu.Item>
+                        </Menu.ItemGroup>
+                    </SubMenu>
+                </Menu>
             </div>
-        </Desktop>
+        </div>
     );
 }
 

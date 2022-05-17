73e595f15c3b8f799a01cd40f6009322390a6830
avrham <aneeman2010@gmail.com>
2022-05-17 15:47:14 +0300

    Bug fixed

--- src/config.js
+++ src/config.js
@@ -13,9 +13,9 @@
 
 const environment = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
 const SERVER_URL_PRODUCTION = `${window.location.href.split('//')[1].split('/')[0]}/api`;
-const SSL_PREFIX = window.location.protocol == 'https:' ? 'https' : 'http';
+const SSL_PREFIX = window.location.protocol == 'https:' ? 'https://' : 'http://';
 
-export const SERVER_URL = environment === 'production' ? `${SSL_PREFIX}/${SERVER_URL_PRODUCTION}` : 'http://localhost:5555/api';
+export const SERVER_URL = environment !== 'production' ? `${SSL_PREFIX}${SERVER_URL_PRODUCTION}` : 'http://localhost:5555/api';
 
 export const HANDLE_REFRESH_INTERVAL = 600000;
 export const SHOWABLE_ERROR_STATUS_CODE = 666;

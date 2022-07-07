5b07263abb73d5a3f365835d9fbbe1b68b40afa9
shay23b <shay.b@strech.io>
2022-07-07 16:41:17 +0300

    removed log

--- src/App.js
+++ src/App.js
@@ -56,7 +56,6 @@ const App = withRouter(() => {
     useEffect(async () => {
         await handleRefresh(true);
         setAuthCheck(false);
-        console.log(process.env.REACT_APP_SANDBOX_ENV);
 
         const interval = setInterval(() => {
             handleRefresh(false);

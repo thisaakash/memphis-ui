11bb708108b9a27af7c88ad53d59f11d454327f7
avrhamNeeman <avraham@strech.io>
2022-07-08 13:10:28 +0300

    New list

--- src/domain/stationOverview/stationObservabilty/messages/index.js
+++ src/domain/stationOverview/stationObservabilty/messages/index.js
@@ -19,24 +19,22 @@ import OverflowTip from '../../../../components/tooltip/overflowtip';
 import { convertBytes, parsingDate } from '../../../../services/valueConvertor';
 import { InfoOutlined } from '@material-ui/icons';
 import CustomTabs from '../../../../components/Tabs';
-import { Divider } from 'antd';
+import { Divider, Table } from 'antd';
+import Button from '../../../../components/button';
 
 const messagesColumns = [
     {
-        key: '1',
-        title: 'Produced by',
-        width: '100px'
+        title: 'Message',
+        dataIndex: 'message'
     },
     {
-        key: '2',
-        title: 'Date',
-        width: '150px'
+        title: 'Details'
     }
 ];
 
 const Messages = () => {
     const [stationState, stationDispatch] = useContext(StationStoreContext);
-    const [selectedRowIndex, setSelectedRowIndex] = useState(0);
+    const [selectedRowIndex, setSelectedRowIndex] = useState([]);
     const [tabValue, setTabValue] = useState(0);
     const tabs = ['All', 'Poison'];
 
@@ -48,53 +46,95 @@ const Messages = () => {
         setTabValue(newValue);
     };
 
+    const rowSelection = {
+        selectedRowIndex,
+        onChange: onSelectedRow
+    };
+
     return (
         <div className="messages-container">
             <div className="header">
-                <p className="title">Station</p>
-                {stationState?.stationSocketData?.messages?.length > 0 && (
-                    <div className="messages-amount">
-                        <InfoOutlined />
-                        <p>Showing last {stationState?.stationSocketData?.messages?.length} messages</p>
-                    </div>
-                )}
+                <div className="left-side">
+                    <p className="title">Station</p>
+                    {stationState?.stationSocketData?.messages?.length > 0 && (
+                        <div className="messages-amount">
+                            <InfoOutlined />
+                            <p>Showing last {stationState?.stationSocketData?.messages?.length} messages</p>
+                        </div>
+                    )}
+                </div>
+                <div className="right-side">
+                    <Button
+                        width="80px"
+                        height="32px"
+                        placeholder="Ack"
+                        colorType="white"
+                        radiusType="circle"
+                        backgroundColorType="purple"
+                        fontSize="12px"
+                        fontWeight="600"
+                        onClick={() => {}}
+                    />
+                    <Button
+                        width="100px"
+                        height="32px"
+                        placeholder="Resend"
+                        colorType="white"
+                        radiusType="circle"
+                        backgroundColorType="purple"
+                        fontSize="12px"
+                        fontWeight="600"
+                        onClick={() => {}}
+                    />
+                </div>
             </div>
             <CustomTabs value={tabValue} onChange={handleChangeMenuItem} tabs={tabs}></CustomTabs>
             {stationState?.stationSocketData?.messages?.length > 0 ? (
-                <div className="list-wrapper">
-                    <div className="list">
-                        <div className="coulmns-table">
-                            {messagesColumns?.map((column, index) => {
-                                return (
-                                    <span key={index} style={{ width: column.width }}>
-                                        {column.title}
-                                    </span>
-                                );
-                            })}
-                        </div>
-                        <div className="rows-wrapper">
-                            {stationState?.stationSocketData?.messages?.map((row, index) => {
-                                return (
-                                    <div className={selectedRowIndex === index ? 'pubSub-row selected' : 'pubSub-row'} key={index} onClick={() => onSelectedRow(index)}>
-                                        <OverflowTip text={row?.produced_by} width={'100px'}>
-                                            {row?.produced_by}
-                                        </OverflowTip>
-                                        <OverflowTip text={parsingDate(row?.creation_date)} width={'150px'}>
-                                            {parsingDate(row?.creation_date)}
-                                        </OverflowTip>
-                                    </div>
-                                );
-                            })}
-                            {!stationState?.stationSocketData?.messages && (
-                                <div className="empty-messages">
-                                    <p>Waiting for messages</p>
-                                </div>
-                            )}
-                        </div>
-                    </div>
-                    <div className="row-data">
-                        {stationState?.stationSocketData.messages && <p>{stationState?.stationSocketData.messages[selectedRowIndex]?.message}</p>}
-                    </div>
+                // <div className="list-wrapper">
+                //     <div className="list">
+                //         <div className="coulmns-table">
+                //             {messagesColumns?.map((column, index) => {
+                //                 return (
+                //                     <span key={index} style={{ width: column.width }}>
+                //                         {column.title}
+                //                     </span>
+                //                 );
+                //             })}
+                //         </div>
+                //         <div className="rows-wrapper">
+                //             {stationState?.stationSocketData?.messages?.map((row, index) => {
+                //                 return (
+                //                     <div className={selectedRowIndex === index ? 'pubSub-row selected' : 'pubSub-row'} key={index} onClick={() => onSelectedRow(index)}>
+                //                         <OverflowTip text={row?.produced_by} width={'100px'}>
+                //                             {row?.produced_by}
+                //                         </OverflowTip>
+                //                         <OverflowTip text={parsingDate(row?.creation_date)} width={'150px'}>
+                //                             {parsingDate(row?.creation_date)}
+                //                         </OverflowTip>
+                //                     </div>
+                //                 );
+                //             })}
+                //             {!stationState?.stationSocketData?.messages && (
+                //                 <div className="empty-messages">
+                //                     <p>Waiting for messages</p>
+                //                 </div>
+                //             )}
+                //         </div>
+                //     </div>
+                //     <div className="row-data">
+                //         { && <p>{stationState?.stationSocketData.messages[selectedRowIndex]?.message}</p>}
+                //     </div>
+                // </div>stationState?.stationSocketData.messages
+                <div>
+                    <Table
+                        rowSelection={rowSelection}
+                        columns={messagesColumns}
+                        dataSource={stationState?.stationSocketData.messages}
+                        pagination={false}
+                        scroll={{
+                            y: 500
+                        }}
+                    />
                 </div>
             ) : (
                 <div className="empty-messages">
